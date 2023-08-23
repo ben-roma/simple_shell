0a37e4302cdf4b77ca8efdc749e18ff6b9169ee1
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-23 15:14:45 +0200

    Create afficher_invite.c

---
+++ afficher_invite.c
@@ -0,0 +1,12 @@
+#include "simple_shell.h"
+#include <stdio.h>
+
+/**
+ * afficher_invite - display prompt
+ * Return: no return
+**/
+void afficher_invite(void)
+{
+	printf("#cisfun$ ");
+	fflush(stdout);
+}
