fa890d24b51fc24bb7cc021294a25216eccbba08
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-26 04:03:20 +0200

    Create trim.c

---
+++ trim.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * trim - delete space
+ * @str: xoxo
+ * Return: str
+**/
+
+char *trim(char *str)
+{
+	char *fin;
+
+	while (isspace((unsigned char)*str))
+		str++;
+
+	if (*str == 0)
+		return (str);
+
+	fin = str + strlen(str) - 1;
+	while (fin > str && isspace((unsigned char)*fin))
+		fin--;
+
+	*(fin + 1) = 0;
+
+	return (str);
+}
+
