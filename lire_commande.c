19c8a42ffa47039c0d881878a3acd7869e7ebcb0
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-26 04:02:49 +0200

    Update lire_commande.c

--- lire_commande.c
+++ lire_commande.c
@@ -7,9 +7,12 @@
  * lire_commande - read a command
  * Return: a ligne.
 **/
+
+
 char *lire_commande(void)
 {
 	char *ligne = malloc(MAXLINE);
+	char *trimmed_ligne;
 	size_t len;
 
 	if (ligne == NULL)
@@ -28,6 +31,19 @@ char *lire_commande(void)
 	if (len > 0 && ligne[len - 1] == '\n')
 	{
 		ligne[len - 1] = '\0';
+	}
+
+	trimmed_ligne = trim(ligne);
+
+	if (*trimmed_ligne == '\0')
+	{
+		free(ligne);
+		return (NULL);
+	}
+
+	if (trimmed_ligne != ligne)
+	{
+		strcpy(ligne, trimmed_ligne);
 	}
 	return (ligne);
 }
