2aacc3c4d9045329e35c535964c9e4f5c446bdde
ben obame <obameben@yahoo.fr>
2024-04-18 15:31:40 +0200

    update

--- simple_shell.h
+++ simple_shell.h
@@ -1,6 +1,13 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
 #define MAXLINE 1024
 
 extern char **environ;
