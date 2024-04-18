333e19a2e8200f5a7d4fc83188addbc5ac737722
ben obame <obameben@yahoo.fr>
2024-04-18 15:00:07 +0200

    update

--- executer_commande.c
+++ executer_commande.c
@@ -1,41 +1,53 @@
 #include "simple_shell.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 /**
- * executer_commande - to execut a command
- * @commande: the command input
- * Return: no return
+* executer_commande - to execute a command
+* @commande: the command input
+* Return: no return
 **/
-
 void executer_commande(char *commande)
 {
 	pid_t pid;
 	int status;
 	char *argv[2];
 
+	if (strcmp(commande, "exit") == 0)
+	{
+		free(commande);
+		exit(0);
+	}
+
 	argv[0] = commande;
 	argv[1] = NULL;
 	pid = fork();
-	if (pid  == -1)
+	if (pid == -1)
 	{
 		perror("Erreur de fork");
-		exit(1);
+		free(commande);
+		exit(EXIT_FAILURE);
 	}
 
 	if (pid == 0)
 	{
 		if (execve(commande, argv, environ) == -1)
 		{
-			fprintf(stderr, "./shell: No such file or directory\n");
-			exit(1);
+			perror(commande);
+			free(commande);
+			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
-		wait(&status);
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+		free(commande);
 	}
 }
